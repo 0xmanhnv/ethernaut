aec9b27686c875419f31795142f301d51a19bf10
0xManhnv <nguyenmanh0397@gmail.com>
2024-04-20 18:12:02 +0700

    Fitst commit

---
+++ test/Fallback.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "src/Fallback.sol";
+
+contract TestFallback is Test {
+    Fallback c;
+
+    function setUp() public {
+        c = new Fallback();
+    }
+
+    function testBar() public {
+        console.log(address(this));
+        assertEq(uint256(1), uint256(1), "ok");
+    }
+
+    // function testFoo(uint256 x) public {
+    //     vm.assume(x < type(uint128).max);
+    //     assertEq(x + x, x * 2);
+    // }
+}
